c61d5bfbcd915cfc1f4d89ab4119ffce378a7a95
Ekaterina Morozova <morozova1861@mail.ru>
2022-08-04 20:59:55 +0500

    ex002

---
+++ ex002/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите имя");
+Console.WriteLine(Console.ReadLine() + ", привет!");
+int a = 5;
+int b = 2;
+Console.WriteLine(a + b);
+Console.WriteLine(a / b);
+Console.WriteLine();
+Console.WriteLine("Введите имя");
+string name = Console.ReadLine();
+if(name.ToLower() == "сергей") {Console.WriteLine("Ну здравствуй, Сергей...");}
+else {Console.WriteLine(name + ", привет!");}
